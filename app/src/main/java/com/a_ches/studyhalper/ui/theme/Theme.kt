650b8ac96fa1c2d6852f07278db10464e9bd38ba
APopova <a_ches@mail.ru>
2022-05-29 22:44:43 +0300

    init commit

---
+++ app/src/main/java/com/a_ches/studyhalper/ui/theme/Theme.kt
@@ -0,0 +1,34 @@
+package com.a_ches.studyhalper.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+private val DarkColorPalette = darkColors(
+    primary = Purple200,
+    primaryVariant = Purple700,
+    secondary = Teal200,
+    onSecondary = Color.White,
+    onBackground = Color.White,
+    onSurface = Color.White,
+    surface = Surface,
+    background = Background
+)
+
+@Composable
+fun StudyHelperTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
+    val colors = if (darkTheme) {
+        DarkColorPalette
+    } else {
+        DarkColorPalette
+    }
+
+    MaterialTheme(
+        colors = colors,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
